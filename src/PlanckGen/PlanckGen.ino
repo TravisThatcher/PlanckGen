f4c97edd07c7af8eb5736e5e7c0fe29be5225c36
TravisThatcher <recompas@gmail.com>
2014-09-19 16:47:01 -0400

    Optimal math things

--- src/PlanckGen/PlanckGen.ino
+++ src/PlanckGen/PlanckGen.ino
@@ -1,5 +1,7 @@
 #include <MozziGuts.h>
 #include <mozzi_fixmath.h>
+#include <AutoMap.h>
+#include <EventDelay.h>
 
 #define KNOB_PIN 1
 
@@ -9,20 +11,25 @@
 int MODE = 0;
 boolean SAMPLE = false;
 int counter = 0;
+int readCounter = 0;
 const int samplebuflength = 400;
-int samplebuf[samplebuflength];
+
 int varsamplebuflength = samplebuflength;
+
+int samplebuf[samplebuflength];
+
+AutoMap Mapper(0,1024, 0, samplebuflength);
+
 int button_state = HIGH;
 
 void setup(){
-  pinMode(BUTTON_PIN, INPUT);
-  
+  pinMode(BUTTON_PIN, INPUT);  
   startMozzi(CONTROL_RATE);
 }
 
 void addSample(int signal){
   if(SAMPLE){
-    samplebuf[counter]=signal;
+      samplebuf[counter]=signal;
   }
   counter++;
   if(counter>samplebuflength){
@@ -33,14 +40,8 @@ void addSample(int signal){
 void updateControl(){
   
   int knob = mozziAnalogRead(KNOB_PIN); 
-  varsamplebuflength = knob>>2;
-  if(varsamplebuflength > 400){
-     varsamplebuflength = 400; 
-  }
-  if(varsamplebuflength<0){
-   varsamplebuflength=0; 
-  }
-  
+  varsamplebuflength = Mapper(knob);
+ 
   button_state = digitalRead(BUTTON_PIN);
   if(button_state == HIGH){
     SAMPLE = false; 
@@ -56,14 +57,14 @@ int updateAudio(){
   int asig = getAudioInput();  
   int sample = asig;
   addSample(sample);
-  if(SAMPLE){
-    asig = samplebuf[counter%varsamplebuflength];
-//    asig >>=2;
-  }else{
-    
+  if(readCounter>varsamplebuflength){
+    readCounter = 0; 
   }
-  // or signal only
-  //asig = samplebuf[counter%varsamplebuflength];
+  if(SAMPLE){
+//      asig = samplebuf[counter%varsamplebuflength];
+      asig = samplebuf[readCounter];
+  }
+  readCounter++;
   return asig;
 }
 
